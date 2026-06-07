b8a80ad7ca99211b8ef0bddd781d4aa8b06788c5
vigalcon <vgalcon92+github@gmail.com>
2026-06-07 18:32:59 +0100

    feat: max concurrency as defautlr

--- terraform/main.tf
+++ terraform/main.tf
@@ -67,8 +67,7 @@ resource "aws_lambda_function" "main" {
 
   timeout                        = local.timeout
   memory_size                    = local.memory_size
-  reserved_concurrent_executions = 10
-
+  
   environment {
     variables = {
       ENVIRONMENT                  = var.environment
